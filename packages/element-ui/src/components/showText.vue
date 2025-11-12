286cd6667e6fc0a62a62c08a28ae8f470a481336
ming chong <ming2882@qq.com>
2025-11-12 18:31:03 +0800

    showText

--- packages/element-ui/src/components/showText.vue
+++ packages/element-ui/src/components/showText.vue
@@ -1,26 +1,35 @@
 <template>
-    <el-text v-if="showField">{{ value }}</el-text>
+	<div v-html="value" v-if="isHtml"></div>
+	<el-text v-else-if="!isHtml" :type="type" :tag="tag">{{ value }}</el-text>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'showText',
-    data() {
-        return {
-            value: ''
-        };
-    },
-    props: {
-        formCreateInject: Object,
-        showField: String,
-        isHtml: Boolean,
-    },
-    created() {
-        var formData = this.formCreateInject.api.formData();
-        this.value = formData[this.showField] || '';
-    },
+	name: 'showText',
+	data() {
+		return {
+			value: ''
+		};
+	},
+	props: {
+		formCreateInject: Object,
+		_optionType: Number,
+		showField: String,
+		text: String,
+		isHtml: Boolean,
+		type: String,
+		tag: String
+	},
+	created() {
+		if (this.text) {
+			this.value = this.text;
+		} else if (this.showField) {
+			var formData = this.formCreateInject.api.formData();
+			this.value = formData[this.showField] || '';
+		}
+	},
 });
 </script>
 <style></style>
--- packages/element-ui/src/config/rule/showText.js
+++ packages/element-ui/src/config/rule/showText.js
@@ -1,55 +1,108 @@
-import { localeProps } from '../../utils';
+import { ru } from "element-plus/es/locale/index.mjs";
+import { localeOptions, localeProps } from "../../utils";
 
-const label = '显示字段';
-const name = 'showText';
+const label = "显示字段";
+const name = "showText";
 
 export default {
-    menu: 'aide',
-    icon: 'icon-span',
-    label,
-    name,
-    rule({ t }) {
-        return {
-            type: name,
-            native: false,
-            title: '',
-            props: {
-                showField: '',
-                isHtml: false
-            },
-            style: {
-                whiteSpace: 'pre-line',
-                width: '100%',
-            }
-        };
+  menu: "aide",
+  icon: "icon-span",
+  label,
+  name,
+  rule({ t }) {
+    return {
+      type: name,
+      props: {
+        showField: "",
+        isHtml: false,
+      },
+    };
+  },
+  watch: {
+    formCreateNative({ value, rule }) {
+      if (value) {
+        rule.title = "";
+      }
     },
-    watch: {
-        formCreateNative({ value, rule }) {
-            if (value) {
-                rule.title = '';
-            }
-        }
+    _optionType({ value, rule }) {
+      if (value == 2) {
+        rule.props.text = "";
+      } else if (value == 1) {
+        rule.props.showField = "";
+      }
     },
-    props(_, { t }) {
-        return localeProps(t, name + '.props', [{
-            type: 'switch', field: 'formCreateNative', title: '是否显示标题',
-            props: {
-                activeValue: false,
-                inactiveValue: true,
-            },
-            control: [{ value: false, rule: ['formCreateTitle'] }], value: false
-        }, {
-            type: 'input', title: '标题',
-            field: 'formCreateTitle',
-        }, {
-            type: 'input',
-            field: 'showField', title: '显示字段',
-        }, {
-            type: 'switch', field: 'isHtml', title: '是否html',
-            props: {
-                activeValue: true,
-                inactiveValue: false,
-            }, value: false
-        }]);
-    }
+  },
+  props(_, { t }) {
+    return localeProps(t, name + ".props", [
+      {
+        type: "radio",
+        title: "文本类型",
+        field: "_optionType",
+        value: 2,
+        options: [
+          { label: "绑定字段", value: 2 },
+          { label: "文本", value: 1 },
+        ],
+        props: {
+          type: "button",
+        },
+        control: [
+          {
+            value: 2,
+            rule: [
+              {
+                type: "input",
+                field: "showField",
+                title: "显示字段",
+              },
+            ],
+          },
+          {
+            value: 1,
+            rule: [
+              {
+                type: "input",
+                field: "text",
+                title: "文本内容",
+                props: {
+                  type: "textarea",
+                  rows: 3,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: "switch",
+        field: "isHtml",
+        title: "是否html",
+        props: {
+          activeValue: true,
+          inactiveValue: false,
+        },
+        value: false,
+      },
+      {
+        type: "select",
+        field: "type",
+        props: {
+          clearable: true,
+        },
+        options: localeOptions(t, [
+          { label: "primary", value: "primary" },
+          { label: "success", value: "success" },
+          { label: "warning", value: "warning" },
+          { label: "danger", value: "danger" },
+          { label: "info", value: "info" },
+        ]),
+      },
+      {
+        type: "input",
+        field: "tag",
+        title: "显示标签",
+        value: "span",
+      },
+    ]);
+  },
 };
